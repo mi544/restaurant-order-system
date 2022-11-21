f344323a68165229b324a06cdc42ea8718ed8366
Maksim Verkhoturov <sd32@protonmail.com>
2022-11-21 11:02:30 -0700

    adds helmet middleware, moduleResolution next->cjs

--- src/utils/initSetup.ts
+++ src/utils/initSetup.ts
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+import helmet from 'helmet'
 
 const initSetup = (app) => {
   // dotenv
   dotenv.config()
   // morgan logger
   app.use(morgan('dev'))
+  app.use(helmet())
 }
 
 export default initSetup
