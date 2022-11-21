0c44dab22644ce27eca3b9645db9be8aa32f68cb
Maksim Verkhoturov <sd32@protonmail.com>
2022-11-21 10:00:40 -0700

    creates init express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello')
+import express from 'express'
+
+const app = express()
+
+app.use('/', (_, res) => {
+  res.json({ hi: 'ho' })
+})
+
+app.listen(1080, () => console.log('listening on 1080'))
